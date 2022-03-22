bae5e564f38b288ea81091772fd4e032a09e5330
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-03-22 16:51:19 +0000

    Update notification_system_covid_19.py

--- notification_system_covid_19.py
+++ notification_system_covid_19.py
@@ -3,3 +3,23 @@ import datetime
 import requests
 
 from plyer import notification
+
+covidData = None
+
+try:
+  
+  covidData = requests.get("https://corona-rest-api.herokuapp.com/Api/uk/")
+  
+except:
+  
+  # if user lacks internet connection
+  
+  print("Error processing data! Please check your internet/data connection!")
+
+if covidData != None:
+  
+  data = CovidData.json()["Success!"]
+  
+  while True:
+    
+    notification.notify()
